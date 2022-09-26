b37eea65c54f43bccc31aa992682d5abcff01c1d
“Nayonna <Nayonna Purnell>
2022-09-26 15:32:35 -0400

    Updated the code with lower and upper case letters

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-
-
-
-
-
 function generatePassword() {
   var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
   var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numericCharacters = "0123456789";
   var specialCharacters = ",.!@#$%^&*";
+  confirmedList ="";
 
   var password = " ";
   
@@ -16,18 +12,42 @@ function generatePassword() {
     "Choose your password length to be between 8 to 128 characters:"
   ));
 
-  //Create the random password based on the password length
-  for (var i = 0; i < passwordLength; i++){
-    password += lowercaseLetters.charAt(Math.floor(Math.random() * lowercaseLetters.length));
-  }
-  
-
   //Checking if the user inputs the correct password length
-  if (!passwordLength >= 8 && !passwordLength <= 128) {
+  if (!passwordLength >= 8 && !passwordLength <= 128){
     alert("Please enter a password between 8 to 128 characters.");
   }
-  console.log("This is the passwordLength: " + passwordLength);
+
+  // Confirm if the user wants to include lowercaseLetters
+  var confirmedLower = window.confirm(
+    "Would you like to include lowercase letters?\nSelect the 'Okay' button if YES"
+  );
+
+  //Move on to the next password option if Cancel
+  if(confirmedLower != true){
+    alert("Okay, moving on to other options");
+    }
+    else{
+      confirmedList = lowercaseLetters;
+    }
   
+  //Do you want to include uppercase?
+  var confirmedUpper = window.confirm(
+    "Would you like to include uppercase letters?"
+  );
+
+   //Move on to the next password option if Cancel
+   if(confirmedUpper!= true){
+    alert("Okay, moving on to other options");
+    }
+    else{
+      confirmedList = lowercaseLetters + uppercaseLetters;
+    }
+
+  //Create the random password based on the password length
+  for (var i = 0; i < passwordLength; i++){
+      password += confirmedList.charAt(Math.floor(Math.random() * confirmedList.length));
+   
+  }
   
 
 
