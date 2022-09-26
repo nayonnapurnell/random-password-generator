7ec5f685a40912877faf3e88a27b3c3c28eeeada
“Nayonna <Nayonna Purnell>
2022-09-26 12:30:18 -0400

    Removed the JavaScript prompts

--- script.js
+++ script.js
@@ -6,56 +6,12 @@ function generatePassword() {
   var specialCharacters = ',.!@#$%^&*';
 
   
-  // Prompt for the password length
-  var passwordLength = window.prompt(
-    "Choose your password length to be between 8 to 128 characters:"
-  );
+  
 
   //Checking if the user inputs the correct password length
   if (!passwordLength >= 8 || !passwordLength <= 128) {
     alert("Please enter a password between 8 to 128 characters.");
   }
-
-    // Prompt for the password length
-    var passwordLength = window.prompt(
-      "Choose your password length to be between 8 to 128 characters:"
-    );
-
-  
-//Confirm if user wants to include lowercase letters?
-  var confirmedLower = window.confirm(
-    "Would you like to include lowercase letters?\nSelect the 'Okay' button if YES"
-  );
-
-  //Move on to the next password option if Cancel
-  if(confirmedLower != true){
-  alert("Okay, moving on to other options");
-  }
- 
-  //Do you want to include uppercase?
-  var confirmedUpper = window.confirm(
-    "Would you like to include uppercase letters?"
-  );
-
-   //Move on to the next password option if Cancel
-   if(confirmedUpper!= true){
-    alert("Okay, moving on to other options");
-    }
-
-  //Do you want to include numeric characters?
-  var confirmedNumeric = window.confirm(
-    "Would you like to include numeric numbers?"
-  );
-
-  if(confirmedNumeric!= true){
-    alert("Okay, moving on to other options");
-    }
-
-  //Do you want special characters?
-  
-  var confirmedCharacters = window.confirm(
-    "Would you like to include special characters?"
-  );
 
   //Create the random password based on the password length
   for (var i = 0; i<=passwordLength; i++){
